a37bb47ce4be33b29197c722ca66df4baae6c408
Darshantada10 <darshantadaofficial@gmail.com>
2023-12-18 15:56:24 +0530

    update

---
+++ PHP/01Intro.php
@@ -0,0 +1,27 @@
+
+
+<h1>
+    .php is an extension we use to create php file
+</h1>
+<h2>
+
+    this is a syntax
+</h2>
+    <h3>
+
+        hello 
+    </h3>
+
+
+<?php 
+
+echo "this is a line from php";
+
+
+?>
+
+
+
+
+php supports html
+but html does not support php
